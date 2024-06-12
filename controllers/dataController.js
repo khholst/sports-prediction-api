9bb99eb6c782b7977d61ff878e083ff4ece0a66c
khholst <karlhendrikholst@gmail.com>
2024-06-12 22:23:10 +0300

    Updated API

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,61 +1,58 @@
 const mongo = require("mongoose");
-const schema = require("./schemas")
-
+const schema = require("./schemas");
 
 exports.tournaments = async (req, res) => {
-    const tournamentCollection = db.model.tournaments || db.model('tournaments', schema.tournament)
-    try {
-        const tournaments = await tournamentCollection.find(req.query).sort({"end_date": "desc"})
-        res.status(200).json(tournaments)
-    } catch (error) {
-        console.log(error)
+  const tournamentCollection = db.model.tournaments || db.model("tournaments", schema.tournament);
+  try {
+    const tournaments = await tournamentCollection.find(req.query).sort({ end_date: "desc" });
+    res.status(200).json(tournaments);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.games = async (req, res) => {
+  const games = db.model("Games", new mongo.Schema({ team1: "string", team2: "string", score1: "number", score2: "number", time: "date", tournament_id: "ObjectID", _id: "ObjectID" }), "games");
+
+  games.find(req.query, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(data);
     }
-}
-
-
-exports.games = async(req, res) => {
-    const games = db.model('Games',
-        new mongo.Schema({team1: 'string', team2: 'string', score1: 'number', score2: 'number', 
-            time: 'date', tournament_id: 'ObjectID',  _id:'ObjectID'}), 'games');
-    
-    games.find(req.query,function(err, data) {
-        if(err){console.log(err);}
-        else{res.json(data);};
-    });
-}
-
+  });
+};
 
 exports.specials = async (req, res) => {
-    try {
-        tournament_id = req.params.id
-        const specialCollection = db.model.specials || db.model('specials', schema.specialPrediction)
-        const specials = await specialCollection.find({tournament_id: tournament_id})
+  try {
+    tournament_id = req.params.id;
+    const specialCollection = db.model.specials || db.model("specials", schema.specialPrediction);
+    const specials = await specialCollection.find({ tournament_id: tournament_id });
 
-        res.status(200).json({
-            code: 200,
-            specials: specials,
-        });
-
-
-    } catch (error) {
-        res.status(500).json({
-            code: 500,
-            errors: [{
-                msg: "Something went wrong with the request"
-            }]
-        });
-    }
-}
-
-
-
-exports.countries = async(req, res) => {
-    let countries = db.model('Countries', 
-        new mongo.Schema({ country_id: 'string', name: 'string',  _id:'ObjectId' }), 'countries'); 
-
-    countries.find({}, function(err, data) { 
-        if(err){console.log(err);}
-        else{
-            res.json(data);}; 
+    res.status(200).json({
+      code: 200,
+      specials: specials,
     });
-}
+  } catch (error) {
+    res.status(500).json({
+      code: 500,
+      errors: [
+        {
+          msg: "Something went wrong with the request",
+        },
+      ],
+    });
+  }
+};
+
+exports.countries = async (req, res) => {
+  let countries = db.model("Countries", new mongo.Schema({ country_id: "string", name: "string", _id: "ObjectId" }), "countries");
+
+  countries.find({}, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(data);
+    }
+  });
+};
--- controllers/resultsController.js
+++ controllers/resultsController.js
@@ -1,215 +1,168 @@
 const mongo = require("mongoose");
-const schema = require("./schemas")
-
-
+const schema = require("./schemas");
 
 function createPointRangeArray(score, pointRange, offset) {
-    const pointRangeArray = [];
-    if (offset === 0) {
-        for (let i = score - pointRange; i < score + pointRange + 1; i++) {
-            pointRangeArray.push(i)
-        }
-    } else {
-        for (let i = score - pointRange; i < score - offset; i++) {
-            pointRangeArray.push(i);
-        }
-        for (let i = score + offset + 1; i < score + pointRange + 1; i++) {
-            pointRangeArray.push(i)
-        }
+  const pointRangeArray = [];
+  if (offset === 0) {
+    for (let i = score - pointRange; i < score + pointRange + 1; i++) {
+      pointRangeArray.push(i);
     }
-    return pointRangeArray
+  } else {
+    for (let i = score - pointRange; i < score - offset; i++) {
+      pointRangeArray.push(i);
+    }
+    for (let i = score + offset + 1; i < score + pointRange + 1; i++) {
+      pointRangeArray.push(i);
+    }
+  }
+  return pointRangeArray;
 }
 
+exports.save = async (req, res) => {
+  try {
+    const result = req.body;
 
+    const tournamentCollection = db.model.tournaments || db.model("tournaments", schema.tournament);
+    const gameCollection = db.model.games || db.model("games", schema.game);
+    const userCollection = db.model.users || db.model("users", schema.user);
 
-exports.save = (async (req, res) => {
+    // Find sport to know which point logic to use
+    const tournament = await tournamentCollection.findOne({ _id: result.tournament_id }, "sport -_id");
+    const sport = tournament.sport;
 
+    // Update prediction in predictions collection with score
+    const predictionWithResult = await gameCollection.updateOne(
+      { _id: result._id },
+      {
+        $set: {
+          score1: result.score1,
+          score2: result.score2,
+        },
+      }
+    );
 
+    // Update user predictions with score
+    let winner = 0;
+    if (result.score1 > result.score2) {
+      winner = 1;
+    } else if (result.score2 > result.score1) {
+      winner = 2;
+    }
+    const difference = Math.abs(result.score1 - result.score2);
 
-    try {
-        const result = req.body;
+    if (sport === "basketball") {
+      const withinFivePointsTeam1 = createPointRangeArray(result.score1, 5, 0);
+      const withinFivePointsTeam2 = createPointRangeArray(result.score2, 5, 0);
 
-        const tournamentCollection = db.model.tournaments || db.model('tournaments', schema.tournament)
-        const gameCollection = db.model.games || db.model('games', schema.game)
-        const userCollection = db.model.users || db.model('users', schema.user)
+      const withinTenPointsTeam1 = createPointRangeArray(result.score1, 10, 0);
+      const withinTenPointsTeam2 = createPointRangeArray(result.score2, 10, 0);
 
-        // Find sport to know which point logic to use
-        const tournament = await tournamentCollection.findOne({_id: result.tournament_id}, "sport -_id")
-        const sport = tournament.sport
+      const savePredictionPoints = await userCollection.bulkWrite([
+        //Update all 0 point predictions
+        {
+          updateMany: {
+            filter: {},
+            update: {
+              $set: { "tournaments.$[tournament].predictions.$[prediction].points": 0 },
+            },
+            arrayFilters: [{ "tournament.tournament_id": mongo.Types.ObjectId(result.tournament_id) }, { "prediction.game_id": mongo.Types.ObjectId(result._id), "prediction.winner": { $ne: winner } }],
+          },
+        },
 
-        // Update prediction in predictions collection with score
-        const predictionWithResult = await gameCollection.updateOne(
-            { _id: result._id },
-            {
-                "$set": {
-                    "score1": result.score1, 
-                    "score2": result.score2
-                }
-            }
-        );
+        //Update all 1 point predictions
+        {
+          updateMany: {
+            filter: {},
+            update: {
+              $set: { "tournaments.$[tournament].predictions.$[prediction].points": 1 },
+            },
+            arrayFilters: [{ "tournament.tournament_id": mongo.Types.ObjectId(result.tournament_id) }, { "prediction.game_id": mongo.Types.ObjectId(result._id), "prediction.winner": winner }],
+          },
+        },
 
+        //Update all 2 point predictions
+        {
+          updateMany: {
+            filter: {},
+            update: {
+              $set: { "tournaments.$[tournament].predictions.$[prediction].points": 2 },
+            },
+            arrayFilters: [{ "tournament.tournament_id": mongo.Types.ObjectId(result.tournament_id) }, { "prediction.game_id": mongo.Types.ObjectId(result._id), "prediction.winner": winner, "prediction.score1": { $in: withinTenPointsTeam1 }, "prediction.score2": { $in: withinTenPointsTeam2 } }],
+          },
+        },
 
-        // Update user predictions with score
-        let winner = 0;
-        if (result.score1 > result.score2) { winner = 1 }
-        else if (result.score2 > result.score1) { winner = 2 }
-        const difference = Math.abs(result.score1 - result.score2);
+        //Update all 3 point predictions
+        {
+          updateMany: {
+            filter: {},
+            update: {
+              $set: { "tournaments.$[tournament].predictions.$[prediction].points": 3 },
+            },
+            arrayFilters: [{ "tournament.tournament_id": mongo.Types.ObjectId(result.tournament_id) }, { "prediction.game_id": mongo.Types.ObjectId(result._id), "prediction.winner": winner, "prediction.score1": { $in: withinFivePointsTeam1 }, "prediction.score2": { $in: withinFivePointsTeam2 } }],
+          },
+        },
+      ]);
+    } else if (sport === "football" || sport == "volleyball") {
+      const savePredictionPoints = await userCollection.bulkWrite([
+        //Update all 2 point predictions
+        {
+          updateMany: {
+            filter: {},
+            update: {
+              $set: { "tournaments.$[tournament].predictions.$[prediction].points": 2 },
+            },
+            arrayFilters: [{ "tournament.tournament_id": mongo.Types.ObjectId(result.tournament_id) }, { "prediction.game_id": mongo.Types.ObjectId(result._id), "prediction.winner": winner, "prediction.difference": difference }],
+          },
+        },
 
+        //Update all 3 point predictions
+        {
+          updateMany: {
+            filter: {},
+            update: {
+              $set: { "tournaments.$[tournament].predictions.$[prediction].points": 3 },
+            },
+            arrayFilters: [{ "tournament.tournament_id": mongo.Types.ObjectId(result.tournament_id) }, { "prediction.game_id": mongo.Types.ObjectId(result._id), "prediction.score1": result.score1, "prediction.score2": result.score2 }],
+          },
+        },
 
-        if (sport === "basketball") {
-            const withinFivePointsTeam1 = createPointRangeArray(result.score1, 5, 0);
-            const withinFivePointsTeam2 = createPointRangeArray(result.score2, 5, 0)
-            
-            const withinTenPointsTeam1 = createPointRangeArray(result.score1, 10, 0);
-            const withinTenPointsTeam2 = createPointRangeArray(result.score2, 10, 0);
+        //Update all 1 point predictions
+        {
+          updateMany: {
+            filter: {},
+            update: {
+              $set: { "tournaments.$[tournament].predictions.$[prediction].points": 1 },
+            },
+            arrayFilters: [{ "tournament.tournament_id": mongo.Types.ObjectId(result.tournament_id) }, { "prediction.game_id": mongo.Types.ObjectId(result._id), "prediction.winner": winner, "prediction.difference": { $ne: difference } }],
+          },
+        },
 
+        //Update all 0 point predictions
+        {
+          updateMany: {
+            filter: {},
+            update: {
+              $set: { "tournaments.$[tournament].predictions.$[prediction].points": 0 },
+            },
+            arrayFilters: [{ "tournament.tournament_id": mongo.Types.ObjectId(result.tournament_id) }, { "prediction.game_id": mongo.Types.ObjectId(result._id), "prediction.winner": { $ne: winner } }],
+          },
+        },
+      ]);
+    }
 
-            const savePredictionPoints = await userCollection.bulkWrite([
-                //Update all 0 point predictions
-                { "updateMany": {
-                    "filter":{},
-                    "update": {
-                        "$set": {"tournaments.$[tournament].predictions.$[prediction].points": 0 },
-                    },
-                    "arrayFilters": [
-                        {   "tournament.tournament_id": mongo.Types.ObjectId(result.tournament_id) },
-                        {   "prediction.game_id": mongo.Types.ObjectId(result._id),
-                            "prediction.winner": {"$ne": winner},
-                        }                                                           
-                    ]
-                }},
-
-
-                //Update all 1 point predictions
-                { "updateMany": {
-                    "filter":{},
-                    "update": {
-                        "$set": {"tournaments.$[tournament].predictions.$[prediction].points": 1 },
-                    },
-                    "arrayFilters": [
-                        {   "tournament.tournament_id": mongo.Types.ObjectId(result.tournament_id) },
-                        {   "prediction.game_id": mongo.Types.ObjectId(result._id),
-                            "prediction.winner": winner,
-
-                        }                                               
-                    ]
-                }},
-
-
-                //Update all 2 point predictions
-                { "updateMany": {
-                    "filter":{},
-                    "update": {
-                        "$set": {"tournaments.$[tournament].predictions.$[prediction].points": 2 },
-                    },
-                    "arrayFilters": [
-                        {   "tournament.tournament_id": mongo.Types.ObjectId(result.tournament_id) },
-                        {   "prediction.game_id": mongo.Types.ObjectId(result._id),
-                            "prediction.winner": winner,                        
-                            "prediction.score1": {"$in": withinTenPointsTeam1},
-                            "prediction.score2": {"$in": withinTenPointsTeam2},
-                        }                                                           
-                    ]
-                }},
-    
-                //Update all 3 point predictions
-                { "updateMany": {
-                    "filter":{},
-                    "update": {
-                        "$set": {"tournaments.$[tournament].predictions.$[prediction].points": 3 },
-                    },
-                    "arrayFilters": [
-                        {   "tournament.tournament_id": mongo.Types.ObjectId(result.tournament_id) },
-                        {   "prediction.game_id": mongo.Types.ObjectId(result._id),
-                            "prediction.winner": winner,
-                            "prediction.score1": {"$in": withinFivePointsTeam1},
-                            "prediction.score2": {"$in": withinFivePointsTeam2},
-                        }                                                           
-                    ]
-                }},
-            ])
-
-
-
-
-
-        } else if (sport === "football" || sport == "volleyball") {
-            const savePredictionPoints = await Users.bulkWrite([
-                //Update all 2 point predictions
-                { "updateMany": {
-                    "filter":{},
-                    "update": {
-                        "$set": {"tournaments.$[tournament].predictions.$[prediction].points": 2 },
-                    },
-                    "arrayFilters": [
-                        {   "tournament.tournament_id": mongo.Types.ObjectId(result.tournament_id) },
-                        {   "prediction.game_id": mongo.Types.ObjectId(result._id),
-                            "prediction.winner": winner,                        
-                            "prediction.difference": difference
-                        }                                                           
-                    ]
-                }},
-    
-                //Update all 3 point predictions
-                { "updateMany": {
-                    "filter":{},
-                    "update": {
-                        "$set": {"tournaments.$[tournament].predictions.$[prediction].points": 3 },
-                    },
-                    "arrayFilters": [
-                        {   "tournament.tournament_id": mongo.Types.ObjectId(result.tournament_id) },
-                        {   "prediction.game_id": mongo.Types.ObjectId(result._id),
-                            "prediction.score1": result.score1,
-                            "prediction.score2": result.score2
-                        }                                                           
-                    ]
-                }},
-    
-                
-                //Update all 1 point predictions
-                { "updateMany": {
-                    "filter":{},
-                    "update": {
-                        "$set": {"tournaments.$[tournament].predictions.$[prediction].points": 1 },
-                    },
-                    "arrayFilters": [
-                        {   "tournament.tournament_id": mongo.Types.ObjectId(result.tournament_id) },
-                        {   "prediction.game_id": mongo.Types.ObjectId(result._id),
-                            "prediction.winner": winner,
-                            "prediction.difference": {"$ne": difference}
-                        }                                                           
-                    ]
-                }},
-    
-                //Update all 0 point predictions
-                { "updateMany": {
-                    "filter":{},
-                    "update": {
-                        "$set": {"tournaments.$[tournament].predictions.$[prediction].points": 0 },
-                    },
-                    "arrayFilters": [
-                        {   "tournament.tournament_id": mongo.Types.ObjectId(result.tournament_id) },
-                        {   "prediction.game_id": mongo.Types.ObjectId(result._id),
-                            "prediction.winner": {"$ne": winner},
-                        }                                                           
-                    ]
-                }},
-            ])
-        }
-
-    
-        res.status(201).json({
-                code: 201,
-                msg: "Result added",
-            });
-
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            code: 500,
-            errors: [{
-                msg: "Something went wrong with the request"
-            }]
-        })
-    };
-})
+    res.status(201).json({
+      code: 201,
+      msg: "Result added",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      code: 500,
+      errors: [
+        {
+          msg: "Something went wrong with the request",
+        },
+      ],
+    });
+  }
+};

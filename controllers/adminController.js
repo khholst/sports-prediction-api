080038e96a153b88be4b6542757bbf85fc145777
khholst <karlhendrikholst@gmail.com>
2024-07-14 23:01:31 +0300

    update

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,204 +1,178 @@
 const mongo = require("mongoose");
-const schema = require("./schemas")
+const schema = require("./schemas");
 
+exports.newTournament = async (req, res) => {
+  const Tournaments = db.model("Tournaments", new mongo.Schema({ name: "string", start_date: "date", end_date: "date", img_url: "string", num_games: "number", host: "array", sport: "string" }), "tournaments");
 
+  try {
+    const newTournament = await Tournaments.create(req.body);
 
-
-exports.newTournament = (async(req, res) => {
-    const Tournaments = db.model('Tournaments', 
-        new mongo.Schema({ name: 'string', start_date: 'date', end_date: 'date', img_url: 'string', num_games:'number', host:'array', sport: 'string' }), 'tournaments'); 
-
-    try {
-        const newTournament = await Tournaments.create(req.body);
-
-        res.status(201).json({
-            code: 201,
-            msg: "Tournament created",
-        });
-
-    } catch (error) {
-        res.status(500).json({
-            code: 500,
-            errors: [{
-                msg: "Something went wrong with the request"
-            }]
-        })
-    }
-})
-
-
-exports.newGame = (async(req, res) => {
-    try {
-        const userCollection = db.models.users || db.model('users', schema.user);
-        const gameCollection = db.models.games || db.model('games', schema.game);
-
-        req.body.tournament_id = req.params.id;
-        const addedGame = await gameCollection.create(req.body);
-
-        const newPrediction = getNewPrediction(addedGame._id)
-        await addPredictionForUsers(userCollection, newPrediction, req.params.id)
-
-        res.status(201).json({
-            code: 201,
-            msg: "Game added",
-        });
-
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            code: 500,
-            errors: [{
-                msg: "Something went wrong with the request"
-            }]
-        });
-    }
-})
-
-async function addPredictionForUsers(userCollection, prediction, tournament_id, special=false) {
-    let subdocument = "predictions";
-    if (special) { subdocument = "special_predictions" }
-    push_location = `tournaments.$[tournament].${subdocument}`
-
-    await userCollection.updateMany({},
+    res.status(201).json({
+      code: 201,
+      msg: "Tournament created",
+    });
+  } catch (error) {
+    res.status(500).json({
+      code: 500,
+      errors: [
         {
-            "$push": {
-                [push_location]: prediction, 
-            },
+          msg: "Something went wrong with the request",
         },
-        { "arrayFilters": [
-            { "tournament.tournament_id": mongo.Types.ObjectId(tournament_id) },
-        ]}
-    )
+      ],
+    });
+  }
+};
+
+exports.newGame = async (req, res) => {
+  try {
+    const userCollection = db.models.users || db.model("users", schema.user);
+    const gameCollection = db.models.games || db.model("games", schema.game);
+
+    req.body.tournament_id = req.params.id;
+    const addedGame = await gameCollection.create(req.body);
+
+    const newPrediction = getNewPrediction(addedGame._id);
+    await addPredictionForUsers(userCollection, newPrediction, req.params.id);
+
+    res.status(201).json({
+      code: 201,
+      msg: "Game added",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      code: 500,
+      errors: [
+        {
+          msg: "Something went wrong with the request",
+        },
+      ],
+    });
+  }
+};
+
+async function addPredictionForUsers(userCollection, prediction, tournament_id, special = false) {
+  let subdocument = "predictions";
+  if (special) {
+    subdocument = "special_predictions";
+  }
+  push_location = `tournaments.$[tournament].${subdocument}`;
+
+  await userCollection.updateMany(
+    {},
+    {
+      $push: {
+        [push_location]: prediction,
+      },
+    },
+    { arrayFilters: [{ "tournament.tournament_id": mongo.Types.ObjectId(tournament_id) }] }
+  );
 }
 
 function getNewPrediction(game_id) {
-    return {
-        game_id: game_id,
-        score1: -1,
-        score2: -1,
-        points: -999,
-        winner: -1
-    }
+  return {
+    game_id: game_id,
+    score1: -1,
+    score2: -1,
+    points: -999,
+    winner: -1,
+  };
 }
 
-exports.newSpecial = (async(req, res) => {
-    
-    try {
-        tournamentId = req.params.id;
-        const specialCollection = db.model.specials || db.model('specials', schema.specialPrediction);
-        const userCollection = db.models.users || db.model('users', schema.user);
+exports.newSpecial = async (req, res) => {
+  try {
+    tournamentId = req.params.id;
+    const specialCollection = db.model.specials || db.model("specials", schema.specialPrediction);
+    const userCollection = db.models.users || db.model("users", schema.user);
 
-        req.body.tournament_id = tournamentId;
-        const newSpecial = await specialCollection.create(req.body);
-        const newSpecialPrediction = getNewSpecialPrediction(newSpecial._id);
+    req.body.tournament_id = tournamentId;
+    const newSpecial = await specialCollection.create(req.body);
+    const newSpecialPrediction = getNewSpecialPrediction(newSpecial._id);
 
-        addPredictionForUsers(userCollection, newSpecialPrediction, tournamentId, true);
+    addPredictionForUsers(userCollection, newSpecialPrediction, tournamentId, true);
 
-        res.status(201).json({
-            code: 201,
-            msg: "Special prediction added",
-        });
-
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            code: 500,
-            errors: [{
-                msg: "Something went wrong with the request"
-            }]
-        });
-    }
-})
-
+    res.status(201).json({
+      code: 201,
+      msg: "Special prediction added",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      code: 500,
+      errors: [
+        {
+          msg: "Something went wrong with the request",
+        },
+      ],
+    });
+  }
+};
 
 function getNewSpecialPrediction(prediction_id) {
-    return {
-        prediction_id   : prediction_id,
-        user_prediction  : "TBD",
-        user_points      : -999
-    }
+  return {
+    prediction_id: prediction_id,
+    user_prediction: "TBD",
+    user_points: -999,
+  };
 }
 
-
 exports.editSpecial = async (req, res) => {
+  try {
+    const result = req.body.result;
+    const tournamentId = mongo.Types.ObjectId(req.params.tournament_id);
+    const specialId = mongo.Types.ObjectId(req.params.special_id);
 
-
-    try {
-        const result        = req.body.result;
-    const tournamentId  = mongo.Types.ObjectId(req.params.tournament_id);
-    const specialId     = mongo.Types.ObjectId(req.params.special_id);
-
-    const specialCollection = db.model.specials || db.model('specials', schema.specialPrediction);
-    const userCollection = db.models.users || db.model('users', schema.user);
+    const specialCollection = db.model.specials || db.model("specials", schema.specialPrediction);
+    const userCollection = db.models.users || db.model("users", schema.user);
 
     const specialPrediction = await specialCollection.findOneAndUpdate(
-        { _id: specialId },
-        {
-            "$set": {
-                "result": result, 
-            }
-        }
+      { _id: specialId },
+      {
+        $set: {
+          result: result,
+        },
+      }
     );
 
     const points = specialPrediction.points;
 
     const savePredictionPoints = await userCollection.bulkWrite([
-        //Update all accurate predictions
-        { "updateMany": {
-            "filter":{},
-            "update": {
-                "$set": {"tournaments.$[tournament].special_predictions.$[prediction].user_points": points },
-            },
-            "arrayFilters": [
-                {   "tournament.tournament_id": tournamentId },
-                {   "prediction.prediction_id": specialId,
-                    "prediction.user_prediction": result,                   
-                }                   
-            ]
-        }},
+      //Update all accurate predictions
+      {
+        updateMany: {
+          filter: {},
+          update: {
+            $set: { "tournaments.$[tournament].special_predictions.$[specialprediction].user_points": points },
+          },
+          arrayFilters: [{ "tournament.tournament_id": tournamentId }, { "specialprediction.prediction_id": specialId, "specialprediction.user_prediction": result }],
+        },
+      },
 
-        //Update all wrong predictions
-        { "updateMany": {
-            "filter":{},
-            "update": {
-                "$set": {"tournaments.$[tournament].special_predictions.$[prediction].user_points": 0 },
-            },
-            "arrayFilters": [
-                {   "tournament.tournament_id": tournamentId },
-                {   "prediction.prediction_id": specialId,
-                    "prediction.user_prediction": {"$ne": result},
-                }
-            ]
-        }},
-    ])
+      // //Update all wrong predictions
+      {
+        updateMany: {
+          filter: {},
+          update: {
+            $set: { "tournaments.$[tournament].special_predictions.$[specialprediction].user_points": 0 },
+          },
+          arrayFilters: [{ "tournament.tournament_id": tournamentId }, { "specialprediction.prediction_id": specialId, "specialprediction.user_prediction": { $ne: result } }],
+        },
+      },
+    ]);
 
     res.status(201).json({
-        code: 201,
-        msg: "Result added",
+      code: 201,
+      msg: "Result added",
     });
-
-
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({
-            code: 500,
-            errors: [{
-                msg: "Something went wrong with the request"
-            }]
-        })
-    }
-
-
-
-
-    
-
-
-
-
-
-
-
-
-}
-
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      code: 500,
+      errors: [
+        {
+          msg: "Something went wrong with the request",
+        },
+      ],
+    });
+  }
+};
--- controllers/schemas.js
+++ controllers/schemas.js
@@ -1,72 +1,68 @@
 const mongo = require("mongoose");
 
-
-
-
 const tournamentSchema = new mongo.Schema({
-    _id         : 'ObjectID',
-    name        : 'String',
-    start_date  : 'Date',
-    end_date    : 'Date',
-    img_url     : 'String',
-    host        : 'Array',
-    num_games   : 'Number',
-    sport       : 'String'
-})
+  _id: "ObjectID",
+  name: "String",
+  start_date: "Date",
+  end_date: "Date",
+  img_url: "String",
+  host: "Array",
+  num_games: "Number",
+  sport: "String",
+});
 exports.tournament = tournamentSchema;
 
 const gameSchema = new mongo.Schema({
-    team1           : 'string',
-    team2           : 'string',
-    score1          : 'number',
-    score2          : 'number', 
-    time            : 'date',
-    tournament_id   : 'ObjectID',
-    stage           : 'string'
+  team1: "string",
+  team2: "string",
+  score1: "number",
+  score2: "number",
+  time: "date",
+  tournament_id: "ObjectID",
+  stage: "string",
 });
 exports.game = gameSchema;
 
 const predictionSchema = {
-    game_id : { type: 'ObjectID', ref: 'Games' },
-    score1  : 'number',
-    score2  : 'number',
-    points  : 'number'
+  game_id: { type: "ObjectID", ref: "Games" },
+  score1: "number",
+  score2: "number",
+  points: "number",
 };
 
-
 const specialPredictionSchema = new mongo.Schema({
-    tournament_id   : 'ObjectID',
-    prediction      : 'string',
-    description     : 'string',
-    points          : 'number',
-    type            : 'string',
-    active_until    : 'string',
-    result          : 'string'
+  tournament_id: "ObjectID",
+  prediction: "string",
+  description: "string",
+  points: "number",
+  type: "string",
+  active_until: "string",
+  result: "string",
 });
 exports.specialPrediction = specialPredictionSchema;
 
-
 const specialPredictionUnderTournamentSchema = {
-    prediction_id   : {type: 'ObjectID', ref: 'specials'},
-    user_prediction : 'string',
-    user_points     : 'number',
-    
-}
-
+  prediction_id: { type: "ObjectID", ref: "specials" },
+  user_prediction: "string",
+  user_points: "number",
+  prediction: "string",
+  result: "string",
+  tournament_id: "ObjectID",
+  end_date: "date",
+};
 
 const tournamentUnderUserSchema = {
-    tournament_id   : {type: "ObjectID", ref: "tournaments"},
-    predictions     : [predictionSchema],
-    special_predictions: [specialPredictionUnderTournamentSchema]
-}
-
+  tournament_id: { type: "ObjectID", ref: "tournaments" },
+  predictions: [predictionSchema],
+  special_predictions: [specialPredictionUnderTournamentSchema],
+};
 
 const userSchema = new mongo.Schema({
-    _id         : 'ObjectID',
-    username    : 'string',
-    password    : 'string',
-    rooms       : 'array',
-    tournaments : [tournamentUnderUserSchema],
-    is_admin    : 'boolean'
+  _id: "ObjectID",
+  username: "string",
+  password: "string",
+  rooms: "array",
+  tournaments: [tournamentUnderUserSchema],
+  is_admin: "boolean",
 });
 exports.user = userSchema;
